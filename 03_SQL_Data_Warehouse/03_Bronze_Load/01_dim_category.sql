270465fe2e3d6cadff6f8156101e1dc509478bce
Mampers <36115147+Mampers@users.noreply.github.com>
2026-02-19 13:51:15 +0000

    Update 01_dim_category.sql

--- 03_SQL_Data_Warehouse/03_Bronze_Load/01_dim_category.sql
+++ 03_SQL_Data_Warehouse/03_Bronze_Load/01_dim_category.sql
@@ -1,1 +1,22 @@
+USE [PalthanioRestaurants]
+GO
 
+/****** Object:  Table [bronze].[dim_category_raw]    Script Date: 19/02/2026 13:48:26 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [bronze].[dim_category_raw](
+	[CategoryKey] [varchar](20) NULL,
+	[CategoryName] [varchar](255) NULL,
+	[Department] [varchar](100) NULL,
+	[SourceSystem] [varchar](50) NULL,
+	[SourceFile] [varchar](260) NULL,
+	[LoadDts] [datetime2](0) NOT NULL
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [bronze].[dim_category_raw] ADD  CONSTRAINT [DF_bronze_dim_category_raw_LoadDts]  DEFAULT (sysdatetime()) FOR [LoadDts]
+GO
