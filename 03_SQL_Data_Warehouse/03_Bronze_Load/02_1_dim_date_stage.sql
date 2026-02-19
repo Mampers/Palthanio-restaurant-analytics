9d5953aac64f38280781e3e67b8cd974a5ae1cc0
Mampers <36115147+Mampers@users.noreply.github.com>
2026-02-19 14:16:47 +0000

    Update 02_1_dim_date_stage.sql

--- 03_SQL_Data_Warehouse/03_Bronze_Load/02_1_dim_date_stage.sql
+++ 03_SQL_Data_Warehouse/03_Bronze_Load/02_1_dim_date_stage.sql
@@ -1,1 +1,44 @@
+/*
+Bronze STAGE: dim_date_stage
+Purpose: Landing table that mirrors the CSV structure for BULK INSERT.
+*/
+
+USE [PalthanioRestaurants];
+GO
+
+DROP TABLE IF EXISTS bronze.dim_date_stage;
+GO
+
+CREATE TABLE bronze.dim_date_stage (
+    DateKey     VARCHAR(20) NULL,
+    FullDate    VARCHAR(30) NULL,
+    [Year]      VARCHAR(10) NULL,
+    [Quarter]   VARCHAR(10) NULL,
+    [Month]     VARCHAR(10) NULL,
+    MonthName   VARCHAR(20) NULL,
+    [Day]       VARCHAR(10) NULL,
+    DayOfWeek   VARCHAR(10) NULL,
+    DayName     VARCHAR(20) NULL,
+    IsWeekend   VARCHAR(10) NULL,
+    WeekOfYear  VARCHAR(10) NULL
+);
+GO
+
+/*
+BULK INSERT TEMPLATE (uncomment and update path locally)
+
+Tip:
+- If your CSV uses Windows line endings, use ROWTERMINATOR = '0x0D0A'
+- If you get encoding issues, keep CODEPAGE = '65001'
+
+BULK INSERT bronze.dim_date_stage
+FROM 'C:\YourPath\dim_date.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '0x0A',
+    TABLOCK,
+    CODEPAGE = '65001'
+);
+*/
 
